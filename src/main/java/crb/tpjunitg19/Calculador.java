b5b634e9a88ea5e34bc5684912f7aaf995c077d9
bordonz <rodrigozbordon@gmail.com>
2026-06-14 17:28:45 -0300

    Metodos en clase calculador

--- src/main/java/crb/tpjunitg19/Calculador.java
+++ src/main/java/crb/tpjunitg19/Calculador.java
@@ -6,5 +6,23 @@ package crb.tpjunitg19;
  * @author BENEGAS GONZALEZ CRISTIAN RENE
  */
 public class Calculador {
-
+    public double suma(double num1, double num2) {
+        double resultado = num1 + num2;
+        return resultado;
+    }
+    
+    public double resta(double num1, double num2) {
+        double resultado = num1 - num2;
+        return resultado;
+    }
+    
+    public int multiplicar(int num1, int num2) {
+        int resultado = num1 * num2;
+        return resultado;
+    }
+    
+    public int dividir(int num1, int num2) {
+        int resultado = num1 / num2;
+        return resultado;
+    }
 }
